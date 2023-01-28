0005797c0a6ee5460275b3765261e1fb12750eee
l.ngankemngankem <l.ngankemngankem@nexidigital.eu>
2023-01-28 12:14:47 +0100

    initial commit

---
+++ src/main/java/com/example/testme/TestmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.testme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testme/TestmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
